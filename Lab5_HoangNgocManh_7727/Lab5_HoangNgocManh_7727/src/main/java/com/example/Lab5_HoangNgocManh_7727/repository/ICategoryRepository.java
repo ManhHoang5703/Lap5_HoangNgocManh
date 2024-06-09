9407b9de7489ac5c0f7eb0f0a8c6ff710e97d31b
ManhHoang5703 <concacdisssmoem@gmail.com>
2024-06-09 10:48:45 +0700

    Lab5

---
+++ Lab5_HoangNgocManh_7727/Lab5_HoangNgocManh_7727/src/main/java/com/example/Lab5_HoangNgocManh_7727/repository/ICategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.Lab5_HoangNgocManh_7727.repository;
+
+import com.example.Lab5_HoangNgocManh_7727.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryRepository extends JpaRepository<Category, Long> {
+}
